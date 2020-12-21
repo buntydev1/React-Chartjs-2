f65791b4ffb3b52c195a455e2784392466f3bab0
yasin <buntyvirani82@gmail.com>
2020-12-20 23:38:58 -0800

    chart updated with bar

--- reactcharts/src/components/Chart.js
+++ reactcharts/src/components/Chart.js
@@ -1,15 +1,16 @@
-import React, {Component} from 'react';
-// import {Bar,Line,Pie} from 'react-chartjs-2';
+import React, { Component } from "react";
+import {Bar,Line,Pie} from 'react-chartjs-2';
 
-
-class Chart extends Component{
-    render(){
-        return(
-            <div className="chart">
-                chart component
-            </div>
-        )
-    }
+class Chart extends Component {
+    
+  render() {
+    return (
+      <Bar
+        data={data}
+        options={{ maintainAspectRatio: false }}
+      />
+    );
+  }
 }
 
 export default Chart;
