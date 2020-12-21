8ce54b42ddd4e071951e03e2ad4d7a0efeeafc54
yasin <buntyvirani82@gmail.com>
2020-12-21 00:56:53 -0800

    the data is passed as props in chartdata

--- reactcharts/src/components/Chart.js
+++ reactcharts/src/components/Chart.js
@@ -5,30 +5,7 @@ class Chart extends Component {
     constructor(props){
         super(props);
         this.state = {
-            chartData:{
-                labels: ['Boston','Worcester','Springfield','Lowell','Cambridge','New Bedford'],
-                datasets:[
-                    {
-                        label:'Population',
-                        data:[
-                            617594,
-	                        181045,
-	                        153060,
-	                        106519,
-	                        105162,
-	                        95072
-                        ],
-                        backgroundColor:[
-                            'rgb(153, 102, 102)',
-                            'rgb(242, 13, 13)',
-                            'rgb(102, 0, 0)',
-                            'rgb(0, 255, 255)',
-                            'rgb(0, 255, 0)',
-                            'rgb(255, 128, 0)'
-                        ]
-                    }
-                ]
-            }
+            chartData:props.chartData
         }
     }
 
