2b517368c427ac8712f32dc1f3ef78ab629b8feb
yasin <buntyvirani82@gmail.com>
2020-12-21 00:55:09 -0800

    data passed in getchartdata whenever it call it will update state

--- reactcharts/src/App.js
+++ reactcharts/src/App.js
@@ -10,13 +10,39 @@ class App extends Component {
       chartData: {}
     }
   }
+  componentWillMount(){
+    this.getChartData();
+  }
   getChartData(){
     this.setState({
-
-    });
+      chartData:{
+        labels: ['Boston','Worcester','Springfield','Lowell','Cambridge','New Bedford'],
+        datasets:[
+            {
+                label:'Population',
+                data:[
+                    617594,
+                  181045,
+                  153060,
+                  106519,
+                  105162,
+                  95072
+                ],
+                backgroundColor:[
+                    'rgb(153, 102, 102)',
+                    'rgb(242, 13, 13)',
+                    'rgb(102, 0, 0)',
+                    'rgb(0, 255, 255)',
+                    'rgb(0, 255, 0)',
+                    'rgb(255, 128, 0)'
+                ]
+            }
+        ]
+    }
+    })
   }
   render() {
-    return <Chart />;
+    return <Chart chartData={this.state.chartData} />;
   }
 }
 
