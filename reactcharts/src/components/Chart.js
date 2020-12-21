52ed695fd0f4494106db783b5c2483f3465729b9
yasin <buntyvirani82@gmail.com>
2020-12-20 23:58:54 -0800

    chart bar updated with cities and population

--- reactcharts/src/components/Chart.js
+++ reactcharts/src/components/Chart.js
@@ -1,8 +1,28 @@
 import React, { Component } from "react";
-import {Bar,Line,Pie} from 'react-chartjs-2';
+import {Bar} from 'react-chartjs-2';
 
 class Chart extends Component {
-    
+    constructor(){
+        super(props);
+        this.state = {
+            chartData:{
+                labels: ['Boston','Worcester','Springfield','Lowell','Cambridge','New Bedford'],
+                datasets:[
+                    {
+                        label:'Population',
+                        data:[
+                            617594,
+	                        181045,
+	                        153060,
+	                        106519,
+	                        105162,
+	                        95072
+                        ]
+                    }
+                ]
+            }
+        }
+    }
   render() {
     return (
       <Bar
