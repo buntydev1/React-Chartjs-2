5a1dc69ab8246f122e4e3da987be07ebf62d7ebd
yasin <buntyvirani82@gmail.com>
2020-12-21 00:42:24 -0800

    removed the unnessecary code

--- reactcharts/src/App.js
+++ reactcharts/src/App.js
@@ -1,28 +1,11 @@
-import logo from './logo.svg';
-import React, {Component } from 'react';
-import './App.css';
-import Chart from './components/Chart';
+import React, { Component } from "react";
+import "./App.css";
+import Chart from "./components/Chart";
 
-class  App extends Component {
-  render(){
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <Chart />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+class App extends Component {
+  render() {
+    return <Chart />;
   }
-  
 }
 
 export default App;
